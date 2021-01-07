8b5d489fe8521ef63eabe52dc2c2917d95dcefba
Oriana Laura K González <orianalk@gmail.com>
2021-01-07 00:25:11 +0000

    Created loop for caculator function

--- calculator.py
+++ calculator.py
@@ -5,3 +5,48 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+# define the function
+# while loop:
+# tokenize input
+
+    # if first token is "q" break
+
+    # else
+
+def calculator():
+    while True:
+        read_input = input("Enter your equation: ")
+        tokens = read_input.split(" ")
+        # tokens[1] = num1
+        # tokens[2] = num2
+
+        if "q" in tokens:
+            print("Quit")
+            break
+
+        else:
+            if tokens[0] == "+":  
+                answer = add(float(tokens[1]), float(tokens[2]))
+                print(answer)
+            if tokens[0] == "-":
+                answer = subtract(float(tokens[1]), float(tokens[2]))
+                print(answer)
+            if tokens[0] == "*":
+                answer = multiply(float(tokens[1]), float(tokens[2]))
+                print(answer)
+            if tokens[0] == "/":
+                answer = divide(float(tokens[1]), float(tokens[2]))
+                print(answer)
+            if tokens[0] == "square":
+                answer = square(float(tokens[1]))
+                print(answer)
+            if tokens[0] == "cube":
+                answer = cube(float(tokens[1]))
+                print(answer)
+            if tokens[0] == "pow":
+                answer = power(float(tokens[1]), float(tokens[2]))
+                print(answer)
+            if tokens[0] == "mod":
+                answer = mod(float(tokens[1]), float(tokens[2]))
+                print(answer)
+calculator()
